f2dee07b1f943d1164b21e95f04063419d5b1245
MatthewLuthi <mattluthi97@gmail.com>
2020-06-05 10:43:57 +1200

    Trying bibtex api

--- search.php
+++ search.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/pcooksey/bibtex-js/src/bibtex_js.js"></script>
+</head>
+<body>
+    <bibtex src="bibtex/TDD Articles in Bibtex format.bib"></bibtex>
+    <div id="bibtex_display"></div>
+</body>
+</html>
 <?php
 
 ?>

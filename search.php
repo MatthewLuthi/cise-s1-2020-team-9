159d1276c9bfa455c01281f1825da9e7db269013
MatthewLuthi <mattluthi97@gmail.com>
2020-06-05 17:47:47 +1200

    Making ui changes

--- search.php
+++ search.php
@@ -1,15 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/pcooksey/bibtex-js/src/bibtex_js.js"></script>
 </head>
 <body>
-    <bibtex src="bibtex/TDD Articles in Bibtex format.bib"></bibtex>
-    <!-- <div id="bibtex_display"></div> -->
     <?php
-    for ($x = 0; $x <= 10; $x++) {
-        echo '<div id="bibtex_display"></div>';
-    }
+
     ?>
 </body>
 </html>

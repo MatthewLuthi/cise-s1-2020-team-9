12dd3ba7c82e0c83a794e9dde78457cfe036e157
Azman234 <49059049+Azman234@users.noreply.github.com>
2020-06-12 17:01:04 +1200

    Update sqlinfo.php

--- conf/sqlinfo.php
+++ conf/sqlinfo.php
@@ -1,13 +1,11 @@
 <?php
 
-// Include file with sql details
-// please change to your user, password, and db
-// please put it under /home/<user>/conf/
+// file with sql details
 
 	$sql_host="us-cdbr-east-05.cleardb.net";
 	$sql_user="bb34fbef3bbd78";
 	$sql_pass="c35b5bcb";
-	$sql_db="SEER";
-	$sql_tble="ArticleInfo";
+	$sql_db="heroku_ec5b6376ec8d01f";
+	$sql_tble="article_info";
 ?>
 
